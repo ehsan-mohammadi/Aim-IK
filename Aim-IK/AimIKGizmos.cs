de68a410c05772f2b26700e3ef0c03b18398396e
unknown <mohammadi.ehsan1994@gmail.com>
2019-08-31 01:50:32 +0430

    Add AimIKGizmos

---
+++ Aim-IK/AimIKGizmos.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using AimIK;
+
+namespace AimIK
+{
+    [RequireComponent(typeof(AimIKBehaviour))]
+    public class AimIKGizmos : MonoBehaviour
+    {
+        AimIKBehaviour aimIKBehaviour;
+
+        /// <summary>
+        /// The Awake function called first of all
+        /// </summary>
+        private void Awake()
+        {
+            // Set the aimIKBehaviour
+            aimIKBehaviour = this.GetComponent<AimIKBehaviour>();
+        }
+
+        /// <summary>
+        /// Draw Gizmos
+        /// </summary>
+        public void OnDrawGizmos()
+        {
+            if(aimIKBehaviour) // If aimIKBehaviour exists
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawLine(aimIKBehaviour.head.position, aimIKBehaviour.target.position);
+            }
+        }
+    }
+}
