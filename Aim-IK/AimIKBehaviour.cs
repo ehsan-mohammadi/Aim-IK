6c6d6c844b3c624a4c4330137b5d9429be0af2eb
unknown <mohammadi.ehsan1994@gmail.com>
2019-08-31 23:43:08 +0430

    Add head clamp

--- Aim-IK/AimIKBehaviour.cs
+++ Aim-IK/AimIKBehaviour.cs
@@ -3,6 +3,7 @@
 namespace AimIK
 {
     using Properties;
+    using Functions;
 
     public class AimIKBehaviour : MonoBehaviour
     {
@@ -12,6 +13,36 @@ namespace AimIK
         public Vector3 eyesOffset;
 
         public LimitRotation headLimitRotation;
+
+        private float headRotationX;
+        private float headRotationY;
+        private float headRotationZ;
+
+        /// <summary>
+        /// Check the clamp
+        /// </summary>
+        private void CheckClamp()
+        {
+            // Clamp (If activate)
+            if (headLimitRotation.xLimitRotation.active)
+                headRotationX = AimIKFunctions.ClampAngle(head.localEulerAngles.x, headLimitRotation.xLimitRotation.min, headLimitRotation.xLimitRotation.max);
+            else
+                headRotationX = head.localEulerAngles.x;
+
+            if (headLimitRotation.yLimitRotation.active)
+                headRotationY = AimIKFunctions.ClampAngle(head.localEulerAngles.y, headLimitRotation.yLimitRotation.min, headLimitRotation.yLimitRotation.max);
+            else
+                headRotationY = head.localEulerAngles.y;
+
+            if (headLimitRotation.zLimitRotation.active)
+                headRotationZ = AimIKFunctions.ClampAngle(head.localEulerAngles.z, headLimitRotation.zLimitRotation.min, headLimitRotation.zLimitRotation.max);
+            else
+                headRotationZ = head.localEulerAngles.z;
+
+            // Set rotation variables to head rotation
+            Vector3 headRotation = new Vector3(headRotationX, headRotationY, headRotationZ);
+            head.localEulerAngles = headRotation;
+        }
         
         /// <summary>
         /// LateUpdate called after Update and FixedUpdate functions each frames. This function is on top of any animation.
@@ -19,6 +50,8 @@ namespace AimIK
         private void LateUpdate()
         {
             head.LookAt(target.position - eyesOffset);
+
+            CheckClamp();
         }
     }
 }
---
+++ Aim-IK/Functions.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+namespace AimIK.Functions
+{
+    public class AimIKFunctions
+    {
+        /// <summary>
+        /// Clamp the angle between min and max
+        /// </summary>
+        /// <param name="angle">The input angle</param>
+        /// <param name="min">The min angle</param>
+        /// <param name="max">The max angle</param>
+        /// <returns>The clamped angle</returns>
+        public static float ClampAngle(float angle, float min, float max)
+        {
+            // Normalize the angle
+            angle = NormalizeAngle(angle);
+
+            // Set angle to correct values
+            if (angle > 180)
+                angle -= 360;
+            else if (angle < -180)
+                angle += 360;
+
+            // Normalize the min
+            min = NormalizeAngle(min);
+
+            // Set min to correct values
+            if (min > 180)
+                min -= 360;
+            else if (min < -180)
+                min += 360;
+
+            // Normalize the max
+            max = NormalizeAngle(max);
+
+            // Set max to the correct values
+            if (max > 180)
+                max -= 360;
+            else if (max < -180)
+                max += 360;
+
+            return Mathf.Clamp(angle, min, max);
+        }
+
+        /// <summary>
+        /// If the angle over 360 or under 360 degree, then normalize them
+        /// </summary>
+        /// <param name="angle">The input angle</param>
+        /// <returns>The normalized angle</returns>
+        private static float NormalizeAngle(float angle)
+        {
+            while (angle > 360)
+                angle -= 360;
+            while (angle < 0)
+                angle += 360;
+
+            return angle;
+        }
+    }
+}
