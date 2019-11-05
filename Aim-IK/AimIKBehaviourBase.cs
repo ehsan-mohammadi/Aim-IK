bccaaccb4eb01561a0026244e04230a631e55df1
unknown <mohammadi.ehsan1994@gmail.com>
2019-11-05 15:52:40 +0330

    Add SmoothLookAt feature

--- Aim-IK/AimIKBehaviourBase.cs
+++ Aim-IK/AimIKBehaviourBase.cs
@@ -8,5 +8,23 @@ namespace AimIK
     public abstract class AimIKBehaviourBase : MonoBehaviour
     {
         public Transform target;
+        public bool smoothLookAt;
+        public float smoothTime;
+        protected Vector3 smoothTarget;
+
+        void Awake()
+        {
+            if (target)
+                smoothTarget = target.position;
+            else
+                smoothTarget = Vector3.zero;
+        }
+
+        void Update()
+        {
+            // Smooth move to target
+            if(target)
+                smoothTarget = Vector3.Lerp(smoothTarget, target.position, smoothTime);
+        }
     }
 }
